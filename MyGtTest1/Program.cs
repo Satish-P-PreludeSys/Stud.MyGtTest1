aafdfae2a3ef285a6feda384652913a92f4b8b93
Satish-P-PreludeSys <163418369+Satish-P-PreludeSys@users.noreply.github.com>
2024-03-15 01:16:31 +0530

    Modified myprogram again from git

--- MyGtTest1/Program.cs
+++ MyGtTest1/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, this is a test application for Gt Evaluation!");
+Console.WriteLine("Hello, this is a test application for Gt Evaluation! Another modification from Git");
